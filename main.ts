a87bdd067329b5ae3e86d94f540605f8fd530dd1
Josep Peiro <epeuva@gmail.com>
2017-04-19 13:11:06 +0200

    Bootstrapping Angular session

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
+/* Compile in the platform */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+/* import our app module */
 import { AppModule } from './app/app.module';
 
+//Attention!! Not a decorator, a function.
 platformBrowserDynamic().bootstrapModule(AppModule);
