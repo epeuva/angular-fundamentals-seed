6f967f950caeaf99d76adf39d58368891d4a3c29
Josep Peiro <epeuva@gmail.com>
2017-04-19 13:01:28 +0200

    Root module with @NgModule session

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,25 @@
+//Module: Container for modules, services, ...
 import { NgModule } from '@angular/core';
+
+// Browser App.
 import { BrowserModule } from '@angular/platform-browser';
 
+// Directives, pipes ...
+import { CommonModule } from '@angular/common';
+
+//Include exported AppComponent
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
+  //Registering AppComponent
   declarations: [
     AppComponent
-  ]
+  ],
+  imports: [
+    BrowserModule, 
+    CommonModule
+  ],
+  //Boostrap App
+  bootstrap: [AppComponent] //rootModule, that is the first angular module,  is the only one that boostrap the application
 })
 export class AppModule {}
