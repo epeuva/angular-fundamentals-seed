8c089afdeb42e998790f1e43e6ea5c937c4e855b
Josep Peiro <epeuva@gmail.com>
2017-04-21 12:34:13 +0200

    Feature modules with @NgModule session

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,16 @@
+/// Angular Modules
 //Module: Container for modules, services, ...
 import { NgModule } from '@angular/core';
-
 // Browser App.
 import { BrowserModule } from '@angular/platform-browser';
-
 // Directives, pipes ...
 import { CommonModule } from '@angular/common';
-
 //Include exported AppComponent
 import { AppComponent } from './app.component';
+
+
+/// Custom Modules
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 @NgModule({
   //Registering AppComponent
@@ -16,8 +18,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, 
-    CommonModule 
+    // Angular modules
+    BrowserModule,
+    CommonModule,
+    // Custom modules
+    PassengerDashboardModule
   ],
   //Boostrap App
   bootstrap: [AppComponent] //rootModule, that is the first angular module,  is the only one that boostrap the application
