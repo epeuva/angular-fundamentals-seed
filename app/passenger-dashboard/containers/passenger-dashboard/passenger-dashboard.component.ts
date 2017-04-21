8e26c788fd41aca3e23f35f84031dc18938e2d2b
Josep Peiro <epeuva@gmail.com>
2017-04-21 14:44:16 +0200

    Presentational (dumb) components session

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Passenger } from './../../modules/passenger.interface';
 
@@ -7,6 +7,8 @@ import { Passenger } from './../../modules/passenger.interface';
   styleUrls: ['passenger-dashboard.component.scss'],
   template: `
     <div class="passenger-dashboard">
+      <passenger-count></passenger-count>
+      <passenger-detail></passenger-detail>
       <h1>Airline Passengers (first method - class.checked-in)</h1>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index;">
@@ -27,66 +29,75 @@ import { Passenger } from './../../modules/passenger.interface';
     </div>
   `
 })
-export class PassengerDashboardComponent {
-  passengers: Passenger[] = [{
-    id: 1,
-    fullName: 'Earl Campbell',
-    checkedIn: false,
-    checkInDate: 1492762309363,
-    children: null
-  }, {
-    id: 2,
-    fullName: 'Wanda Flores',
-    checkedIn: true,
-    checkInDate: 1492722309363,
-    children: [{name:'Lorem', age: 20}, {name:'Ipsum', age: 7}]
-  }, {
-    id: 3,
-    fullName: 'Emily Knight',
-    checkedIn: true,
-    checkInDate: 1492622309363,
-    children: null
-  }, {
-    id: 4,
-    fullName: 'Shawn Howard',
-    checkedIn: true,
-    checkInDate: 1492522309363,
-    children:  [{name:'Sit', age: 30}]
-  }, {
-    id: 5,
-    fullName: 'Paula Wright',
-    checkedIn: false,
-    checkInDate: null,
-    children: null
-  }, {
-    id: 6,
-    fullName: 'Elizabeth Owens',
-    checkedIn: true,
-    checkInDate: 1492322309363,
-    children: null
-  }, {
-    id: 7,
-    fullName: 'Debra Johnson',
-    checkedIn: true,
-    checkInDate: 1492122309363,
-    children: [{name:'Amet', age: 25}]
-  }, {
-    id: 8,
-    fullName: 'Sean Harris',
-    checkedIn: false,
-    checkInDate: 1491922309363,
-    children: null
-  }, {
-    id: 9,
-    fullName: 'Helen Grant',
-    checkedIn: true,
-    checkInDate: 1491622309363,
-    children: null
-  }, {
-    id: 10,
-    fullName: 'Aaron Scott',
-    checkedIn: false,
-    checkInDate: 1491222309363,
-    children: null
-  }]
+export class PassengerDashboardComponent implements OnInit {
+  passengers: Passenger[];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    this.passengers = [{
+      id: 1,
+      fullName: 'Earl Campbell',
+      checkedIn: false,
+      checkInDate: 1492762309363,
+      children: null
+    }, {
+      id: 2,
+      fullName: 'Wanda Flores',
+      checkedIn: true,
+      checkInDate: 1492722309363,
+      children: [{ name: 'Lorem', age: 20 }, { name: 'Ipsum', age: 7 }]
+    }, {
+      id: 3,
+      fullName: 'Emily Knight',
+      checkedIn: true,
+      checkInDate: 1492622309363,
+      children: null
+    }, {
+      id: 4,
+      fullName: 'Shawn Howard',
+      checkedIn: true,
+      checkInDate: 1492522309363,
+      children: [{ name: 'Sit', age: 30 }]
+    }, {
+      id: 5,
+      fullName: 'Paula Wright',
+      checkedIn: false,
+      checkInDate: null,
+      children: null
+    }, {
+      id: 6,
+      fullName: 'Elizabeth Owens',
+      checkedIn: true,
+      checkInDate: 1492322309363,
+      children: null
+    }, {
+      id: 7,
+      fullName: 'Debra Johnson',
+      checkedIn: true,
+      checkInDate: 1492122309363,
+      children: [{ name: 'Amet', age: 25 }]
+    }, {
+      id: 8,
+      fullName: 'Sean Harris',
+      checkedIn: false,
+      checkInDate: 1491922309363,
+      children: null
+    }, {
+      id: 9,
+      fullName: 'Helen Grant',
+      checkedIn: true,
+      checkInDate: 1491622309363,
+      children: null
+    }, {
+      id: 10,
+      fullName: 'Aaron Scott',
+      checkedIn: false,
+      checkInDate: 1491222309363,
+      children: null
+    }]
+  }
 }
