5cec0652cbceb922d88dccf8a01b6bb382690a28
Josep Peiro <epeuva@gmail.com>
2017-04-24 18:17:42 +0200

    Binding to radio buttons session

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -14,6 +14,17 @@ import { Passenger } from './../../modules/passenger.interface';
       <div>
         Passenger Id: <input type="number" name="id" [ngModel]="detail?.id">
       </div>
+      <div>
+        <label>
+          <input type="radio" name="checkedIn" [value]="true" [ngModel]="detail?.checkedIn" (ngModelChange)="toggleCheckIn($event)"> Yes
+        </label>
+        <label>
+          <input type="radio" name="checkedIn" [value]="false" [ngModel]="detail?.checkedIn" (ngModelChange)="toggleCheckIn($event)"> No
+        </label>
+      </div>
+      <div *ngIf="form.value.checkedIn">
+        Check in date: <input type="number" name="checkInDate" [ngModel]="detail?.checkInDate">
+      </div>
       {{ form.value | json }}
     </form>
   `
@@ -22,5 +33,12 @@ export class PassengerFormComponent {
 
   @Input()
   detail: Passenger;
+
+  toggleCheckIn(checkedIn: boolean) {
+    if(checkedIn){
+      //this.detail.checkInDate = +new Date();
+      this.detail.checkInDate = Date.now(); //ms
+    }
+  }
 
 }
